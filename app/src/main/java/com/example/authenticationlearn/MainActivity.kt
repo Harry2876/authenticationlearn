3f9ec2f5e642d2bf06cc8eb76eab9f818ba60100
ʜᴀʀɪᴏᴍ ʜᴀʀꜱʜ <acbcharryxyz@gmail.com>
2024-09-23 22:06:32 +0530

    Initial Release of Login Functionality

--- app/src/main/java/com/example/authenticationlearn/MainActivity.kt
+++ app/src/main/java/com/example/authenticationlearn/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.example.authenticationlearn
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
     var logoutBtn : TextView? = null
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,8 +26,23 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        auth = FirebaseAuth.getInstance()
+
         //Initialising Elements
         logoutBtn = findViewById(R.id.outbtn)
+
+        logoutBtn?.setOnClickListener {
+            // Sign out from Firebase
+            auth.signOut()
+
+            // After logout, redirect the user to the login screen
+            val loginIntent = Intent(this@MainActivity, LoginActivity::class.java)
+            loginIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(loginIntent)
+            finish()
+
+            Toast.makeText(this, "Logout Succesfull", Toast.LENGTH_SHORT).show()
+        }
 
 
 
